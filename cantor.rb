0222abba09d24a83be5417187d1c785caf8bb71b
Travis Herrick <quasimonomial@gmail.com>
2015-02-08 17:13:02 -0800

    initial commit and name Cantor methods

---
+++ cantor.rb
@@ -0,0 +1,18 @@
+class Cantor
+  attr_accessor :raw_integer
+
+  def initialize raw_integer
+    self.raw_integer = raw_integer
+
+  end
+
+  def print
+
+  end
+
+  private
+  def convert_to_cantor
+  end
+  
+
+end
