cae32b07f351dae42260adb1c781ed3060446c14
Travis Herrick <quasimonomial@gmail.com>
2015-02-08 17:39:17 -0800

    Figure out rest of needed methods, add conversion to cantor in the initialization function

--- cantor.rb
+++ cantor.rb
@@ -1,21 +1,27 @@
 class Cantor
   attr_accessor :raw_integer
+  attr_reader :cantor_expansion
 
   def initialize raw_integer
     self.raw_integer = raw_integer
-
+    self.cantor_expansion = self.convert_to_cantor
   end
 
-  def print
-
+  def + other_cantor
+  
   end
 
   def == other_cantor
     other_cantor.raw_integer == self.raw_integer
   end
 
+  def to_s
+    
+  end
+
   private
   def convert_to_cantor
+    
   end
 
 
